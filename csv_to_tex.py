479406bdf09c1ba75b6a544aa99d11138a7d97f6
kitizl <nithesh.dragoon98@gmail.com>
2018-10-16 17:34:45 +0200

    Changed \hline command because it was interfering with the LaTeX syntax.

--- csv_to_tex.py
+++ csv_to_tex.py
@@ -12,11 +12,11 @@ def convert_file(pd_file):
     # adding headers separately
     headers = pd_file.columns
     # this is how the tabular object is made in LaTeX
-    begin_str = "\\begin{tabular}{|"+ "c|"*len(pd_file.columns) +"}\n"
+    begin_str = "\\begin{table} \n \\centering \n\\begin{tabular}{|"+ "c|"*len(pd_file.columns) +"}\n"
     # and this is how the tabular object is terminated
-    end_str = r"\end{tabular}"
+    end_str = "\\end{table}\n \\caption{"+sys.argv[1]+"} \n\\end{tabular}"
     # the headers are the first row of the 'body' of the main table
-    main_str = " & ".join(headers) + " \\\\ \n\\hline \\\\ \n"
+    main_str = " & ".join(headers) + " \\\\ \n\\hline  \n"
     # the following loop will go through the individual rows of the dataframe and
     # connects the individual elements using '&' (because of LaTeX syntax) and
     # then moves ahead with a \\\\ (making a LaTeX newline) and \n (a newline for
